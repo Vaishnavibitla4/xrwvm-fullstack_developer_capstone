d7cfd163bd26368509f7dace31ca24dc3a75ffee
Vaishnavibitla4 <bitlavaishnavi4@gmail.com>
2026-02-27 12:12:29 -0500

    proxy services of backend APIs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
     path('logout', views.logout_request, name='logout'),
     path('register', views.registration, name='register'),
     path('get_cars', views.get_cars, name ='getcars'),
+    path('get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path('add_review', view=views.add_review, name='add_review'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
