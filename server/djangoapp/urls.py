df724c71a6c6a6585a9ecf36bd9c09f3833cdd90
Vaishnavibitla4 <bitlavaishnavi4@gmail.com>
2026-02-27 14:22:13 -0500

    added dynamic pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('logout', views.logout_request, name='logout'),
     path('register', views.registration, name='register'),
     path('get_cars', views.get_cars, name ='getcars'),
-    path('get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path('get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path('get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path('dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path('reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
